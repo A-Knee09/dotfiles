f0a35ca882fb1b48932eb555a62894d7505c4e1e
Anirudh Saksena <anen08saks@gmail.com>
2025-06-22 05:46:28 +0530

    initial  commit

---
+++ nvim/lua/plugins/colorizer.lua
@@ -0,0 +1,12 @@
+-- Colorizer
+return{
+  "norcalli/nvim-colorizer.lua",
+  event = "BufReadPost",
+  config = function()
+    require("colorizer").setup({
+      "*",                     -- Apply colorizer to all file types
+      css = { rgb_fn = true }, -- Enable parsing of rgb(...) functions in CSS
+    })
+  end,
+  lazy = true,
+}
---
+++ nvim/lua/plugins/kanagawa.lua
@@ -0,0 +1,10 @@
+-- Kanagawa
+return{
+  "rebelot/kanagawa.nvim",
+  name = "kanagawa",
+	--  priority = 1000,
+	-- config = function()
+	-- 	vim.cmd.colorscheme("tokyodark")
+	-- end,
+}
+
